9eac8af0723bcdc351785628e15fc4049f1927f4
juanjosecorella <juanjosecorella2004@gmail.com>
2024-06-16 11:59:07 -0600

    cleanup

--- src/Components/GananciasDateRange.tsx
+++ src/Components/GananciasDateRange.tsx
@@ -1,17 +1,14 @@
 import { useForm } from "react-hook-form";
 import LoadState from "../api/LoadState";
-import { useContext } from "react";
-import MyContext from "../Context/MyContext";
 import useGetGananciasDateRange from "../api/useGetGananciasDateRange";
 
 const GananciasDateRange = () => {
     let { register, handleSubmit } = useForm();
 
-    const { routes } = useContext(MyContext);
     const { ganancias, getGanancias } = useGetGananciasDateRange();
 
     return (
-        <div className="ganancias-area">
+        <div className="ganancias-date-area">
             <form onSubmit={handleSubmit(getGanancias)}>
 
                 Consultar ganancias desde la fecha:
